c8347a339571aa9002deec4d3ff824a56d73696f
Jalil Muyeke <muyekejalil@gmail.com>
2025-07-11 17:04:12 +0300

    git add .

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
