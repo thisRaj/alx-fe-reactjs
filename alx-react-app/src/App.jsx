5f4383942bd99c8b6cd8a9fe50ff406278dfb8ce
Jalil Muyeke <muyekejalil@gmail.com>
2025-07-11 14:34:42 +0300

    alx-react-app

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>This far we have come</h1>
+      <h4>React is my new thing</h4>
+    </>
+  )
+}
+
+export default App
