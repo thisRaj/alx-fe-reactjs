511461cb18647373c2dfa2d75feffef59cc53a60
Jalil Muyeke <muyekejalil@gmail.com>
2025-07-11 19:44:14 +0300

    New one

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,20 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   return (
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+  );
+  (
     <div>
       <Header />
       <MainContent />
@@ -16,7 +22,7 @@ function App() {
     </div>
   );
 }
-
+{
 function App() {
   const [count, setCount] = useState(0)
 
@@ -26,6 +32,7 @@ function App() {
       <WelcomeMessage />
     </div>
   );
+}
 }
 
   return 
