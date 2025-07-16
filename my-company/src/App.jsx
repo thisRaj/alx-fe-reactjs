58bb8264d993062aaa35908e1fdab2e538532862
Jalil Muyeke <muyekejalil@gmail.com>
2025-07-16 23:11:37 +0300

    Contact

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Contact from './components/Contact';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
