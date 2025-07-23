4ca8ccc604195b4206137fee821441d9adb35385
Jalil Muyeke <muyekejalil@gmail.com>
2025-07-23 18:20:00 +0300

    New ones

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -21,24 +21,19 @@ function HomePage() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
-      <h1>Vite + React</h1>
+      <h1>Jalil's Recipe Sharing</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This is how we learn to make the best meals!!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Let's save your relationships with food fellas!!
       </p>
 
       <hr />
